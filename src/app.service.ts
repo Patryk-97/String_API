9a0807b91e0283763f2ce1a6a58b8467272c2482
Patryk.Stepien <215886@edu.p.lodz.pl>
2020-11-03 14:53:35 +0100

    Fixed funtions to get some characters (if in string is not any small letters inside function getSmallLetters() then match() function returns null, so calling toString() function for null is invalid)

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,18 +3,23 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getSmallLetters(someString: string): string {
-    return someString.match(/[a-z]/g).toString();
+    let smallLetters = someString.match(/[a-z]/g);
+    return (smallLetters === null ? "" : smallLetters.toString());
   }
 
   getBigLetters(someString: string): string {
-    return someString.match(/[A-Z]/g).toString();
+    let bigLetters = someString.match(/[A-Z]/g).toString();
+    return (bigLetters === null ? "" : bigLetters.toString());
   }
 
   getDigits(someString: string): string {
-    return someString.match(/[0-9]/g).toString();
+    let digits = someString.match(/[0-9]/g).toString();
+    return (digits === null ? "" : digits.toString());
+    
   }
 
   getSpecialCharacters(someString: string): string {
-    return someString.match(/[^a-zA-Z0-9]/g).toString();
+    let specialCharacters = someString.match(/[^a-zA-Z0-9]/g).toString();
+    return (specialCharacters === null ? "" : specialCharacters.toString());
   }
 }
